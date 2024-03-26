92a4b2ea1340ddd2c1f4e94f4a1a2d9c141539c1
PM2600 <3262929659@qq.com>
2024-03-26 20:26:01 +0800

    创建项目

---
+++ VideoPlay/VideoClient/VideoClient.h
@@ -0,0 +1,32 @@
+﻿
+// VideoClient.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CVideoClientApp:
+// 有关此类的实现，请参阅 VideoClient.cpp
+//
+
+class CVideoClientApp : public CWinApp
+{
+public:
+	CVideoClientApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CVideoClientApp theApp;
